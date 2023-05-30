8ba69812a13fdf4b39f7fec647da651ebaecb598
arnaupv <arnau.pont.vilchez@gmail.com>
2023-05-30 19:57:36 +0200

    adding treesitter configuration to have a better control of treesitter

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    opts.ignore_install = { "help" }
+
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, {
+        "bash",
+        "regex",
+        "yaml",
+        "json",
+        "dockerfile",
+        "git_config",
+        "jsdoc",
+        "make",
+        "toml",
+        "vimdoc",
+      })
+    end
+  end,
+}
