ec7ef0032aff7aa39bd7bb8d3f3500c504e1161b
arnaupv <arnau.pont.vilchez@gmail.com>
2023-06-11 21:18:08 +0200

    Nvim: activating pluging for easily debugging neovim

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>uh", "<cmd>checkhealth<cr>", { noremap = true, silent = false, desc = "Check Health" })
+vim.keymap.set(
+  "n",
+  "<leader>ul",
+  "<cmd>:edit $NVIM_LOG_FILE<cr>",
+  { noremap = true, silent = false, desc = "Show LogFile" }
+)
+vim.keymap.set("n", "<leader>um", "<cmd>:messages<cr>", { noremap = true, silent = false, desc = "Show Messages" })
