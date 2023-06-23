3703d7cb2483fd328c714988fe4f10f5942f9694
arnaupv <arnau.pont.vilchez@gmail.com>
2023-06-23 16:43:08 +0200

    using arnaupv/neotest-plenary instead of the original repo (for development purposes). This commit has to be reverted once DAP is working for neotest-plenary and the PR is merged to nvim-neotest/neotest-plenary

--- config/nvim/lua/plugins/test.lua
+++ config/nvim/lua/plugins/test.lua
@@ -1,5 +1,11 @@
 return {
-  { "nvim-neotest/neotest-plenary" },
+  -- { "nvim-neotest/neotest-plenary" },
+  { "arnaupv/neotest-plenary", branch = "make-dap-available-in-the-adapter" },
+  -- {
+  --   dir = "/home/pontsoul/Documents/Lab/NvimPlugins/neotest-plenary/",
+  --   config = true,
+  --   dev = true,
+  -- },
   {
     "nvim-neotest/neotest",
     opts = { adapters = { "neotest-plenary" } },
