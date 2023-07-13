11901c118356e7f707b4b054482fc7a6d6f583d5
Arnau Pont <arnau.pont.vilchez@gmail.com>
2023-07-13 11:30:06 +0200

    Completing the rust installation

--- packages/rust.sh
+++ packages/rust.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-apt-get update
-# Rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+set -ex
 
-rustup override set stable
-rustup update stable
+MY_USER=$(ls /home/ | head -n1)
+MY_HOME="/home/${MY_USER}"
+
+apt-get update
+
+# Rust
+if [ ! -d "${MY_HOME}/.cargo/" ]; then
+	su "${MY_USER}" -c sh -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)"
+	rustup override set stable
+	rustup update stable
+fi
