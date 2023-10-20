f54a17e43bf2e9041351f0a10c021d70af5e7aaa
Arnau Pont <arnau.pont.vilchez@gmail.com>
2023-10-20 13:03:05 +0200

    udpate nodejs installation

--- packages/nodejs.sh
+++ packages/nodejs.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-apt-get update
-apt-get install -y curl wget
-curl -sL https://deb.nodesource.com/setup_18.x | bash -
+# Instructions
+# https://github.com/nodesource/distributions#installation-instructions
 
 apt-get update
-apt-get install -y nodejs
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgkey
+
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+
+apt-get update
+apt-get install nodejs -y
