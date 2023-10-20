0902459c8bec6600aeb3a83507e0199d149b06f3
Arnau Pont <arnau.pont.vilchez@gmail.com>
2023-10-20 13:34:49 +0200

    Downgrading default nodejs installtion from 20 to 18

--- packages/nodejs.sh
+++ packages/nodejs.sh
@@ -8,7 +8,8 @@ apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgkey
 
-NODE_MAJOR=20
+# Let's udpate to NODE_MAJOR=20 when the issue is resolved https://github.com/nodesource/distributions/issues/1688
+NODE_MAJOR=18
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update
