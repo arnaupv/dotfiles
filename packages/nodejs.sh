b1429ca636e4444a47fd13d8fb277809c5fb6deb
Arnau Pont <arnau.pont.vilchez@gmail.com>
2023-10-20 14:01:27 +0200

    Fixing typo in GPG key installation

--- packages/nodejs.sh
+++ packages/nodejs.sh
@@ -3,18 +3,13 @@
 # Instructions
 # https://github.com/nodesource/distributions#installation-instructions
 
-# Pinning Found in https://github.com/nodesource/distributions/issues/1579
-echo "Package: nodejs" >> /etc/apt/preferences.d/preferences && \
-echo "Pin: origin deb.nodesource.com" >> /etc/apt/preferences.d/preferences && \
-echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/preferences
-
 apt-get update
 apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpgkey
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 # Let's udpate to NODE_MAJOR=20 when the issue is resolved https://github.com/nodesource/distributions/issues/1688
-NODE_MAJOR=18
+NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update
